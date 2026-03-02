2e2ce7f2d36554ab2467ad49e4894bb87a984ef9
ana0212 <analuizallmp@gmail.com>
2026-03-02 16:09:31 -0300

    setup railway

--- app/config.py
+++ app/config.py
@@ -3,20 +3,10 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
 class DevelopmentConfig(Config):
-    database_url = os.getenv("DATABASE_URL")
-
-    if database_url:
-        database_url = database_url.replace("postgres://", "postgresql://")
-        SQLALCHEMY_DATABASE_URI = database_url
-    else:
-        SQLALCHEMY_DATABASE_URI = (
-            "postgresql+psycopg2://postgres:senha@localhost:5432/pipeline_imobiliario"
-            "?options=-csearch_path=imobiliario"
-        )
-
+    pass
 
 config = {
     "default": DevelopmentConfig,
