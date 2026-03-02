c30fb60e290d5cabb4cd050809e40c0cd9db7bb4
ana0212 <analuizallmp@gmail.com>
2026-03-02 17:42:13 -0300

    update config.py

--- app/config.py
+++ app/config.py
@@ -4,3 +4,15 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    # Força o schema public
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {"options": "-csearch_path=public"}
+    }
+
+class DevelopmentConfig(Config):
+    pass
+
+config = {
+    "default": DevelopmentConfig,
+    "development": DevelopmentConfig
+}
