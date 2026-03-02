480572e81f57353be2d154202aadeae75daadb7d
ana0212 <analuizallmp@gmail.com>
2026-03-02 17:41:17 -0300

    update config.py

--- app/config.py
+++ app/config.py
@@ -4,11 +4,3 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-
-class DevelopmentConfig(Config):
-    pass
-
-config = {
-    "default": DevelopmentConfig,
-    "development": DevelopmentConfig
-}
